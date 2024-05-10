916485a710df530a9fa740f403134ef506802e53
Skander-HajMabrouk <skander.hajmabrouk@outlook.fr>
2024-05-10 17:55:51 +0100

    chore: Remove unnecessary code and update association details page

--- src/app/associations/association-details/association-details.page.ts
+++ src/app/associations/association-details/association-details.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Association } from 'src/app/interfaces/association';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-association-details',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AssociationDetailsPage implements OnInit {
 
-  constructor() { }
+  selectedAssociation: Association | undefined;
+
+  constructor(private dataService:DataService, private route:ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params=>{
+      const id = params['id'];
+      console.log(id);
+      this.dataService.getAssociationById(id).subscribe(
+        res=>{
+          console.log(res);
+          this.selectedAssociation = res;
+        }
+      )
+    })
   }
 
 }
