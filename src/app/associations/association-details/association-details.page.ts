ef2c83cf915ac84779f42966cf38e771ce690172
Skander Haj Mabrouk <105309293+SkanderHAJMABROUK@users.noreply.github.com>
2024-06-20 20:52:59 +0100

    chore: Update donation success toast messages, toast color, and duration in association and collecte details pages

--- src/app/associations/association-details/association-details.page.ts
+++ src/app/associations/association-details/association-details.page.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, OnChanges, SimpleChanges, ChangeDetectorRef } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnChanges,
+  SimpleChanges,
+  ChangeDetectorRef,
+} from '@angular/core';
 import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 import { Association } from 'src/app/interfaces/association';
 import { DataService } from 'src/app/services/data.service';
@@ -13,22 +19,20 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['./association-details.page.scss'],
 })
 export class AssociationDetailsPage implements OnInit {
-
   selectedAssociation: Association | undefined;
   donationAmount: number = 0;
   paymentSuccessful!: string | null;
-  orderId: string = ''; 
+  orderId: string = '';
   orderStatus: number = 0;
   donateurId: string = '';
   id!: string;
   value = 0;
 
   constructor(
-    private dataService: DataService, 
-    private route: ActivatedRoute, 
+    private dataService: DataService,
+    private route: ActivatedRoute,
     private paymentService: PaymentService,
     public router: Router,
-    private localNotifications: LocalNotifications,
     public toastController: ToastController,
     private cdr: ChangeDetectorRef
   ) {}
@@ -40,13 +44,13 @@ export class AssociationDetailsPage implements OnInit {
   }
 
   ngOnInit(event?: any) {
-    this.route.params.subscribe(params => {
+    this.route.params.subscribe((params) => {
       this.id = params['id'];
       console.log(this.id);
       this.getAssociationById(this.id);
     });
 
-    this.router.events.subscribe(event => {
+    this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
         if (event.url.includes('/association-details/')) {
           this.getAssociationById(this.id);
@@ -59,32 +63,33 @@ export class AssociationDetailsPage implements OnInit {
     }
   }
 
-
   getAssociationById(id: string) {
     this.dataService.getAssociationById(id).subscribe({
       next: (data: Association | undefined) => {
         if (data !== undefined) {
-          this.selectedAssociation = data; 
+          this.selectedAssociation = data;
           localStorage.setItem('service.showDetails', 'true');
         } else {
-          console.error('Erreur: Aucune donnée n\'a été renvoyée.');
+          console.error("Erreur: Aucune donnée n'a été renvoyée.");
         }
       },
-      error: error => {
+      error: (error) => {
         console.error('Erreur lors de la récupération des données :', error);
-      }
+      },
     });
   }
 
   getOrderStatus(orderId: string): void {
-    
-    this.paymentService.getOrderStatus(orderId).subscribe(response => {
-      console.log(response);
-      this.orderStatus = response.OrderStatus as number;
-      console.log('order status in function', this.orderStatus);
-    }, error => {
-      console.error('Error fetching order status:', error);
-    });
+    this.paymentService.getOrderStatus(orderId).subscribe(
+      (response) => {
+        console.log(response);
+        this.orderStatus = response.OrderStatus as number;
+        console.log('order status in function', this.orderStatus);
+      },
+      (error) => {
+        console.error('Error fetching order status:', error);
+      }
+    );
   }
 
   updateDonationAmountFromButton(amount: number) {
@@ -99,48 +104,73 @@ export class AssociationDetailsPage implements OnInit {
     const returnUrl = `example://associations-list/${this.id}`;
     const randomIdentifier = Math.random().toString(36).substring(2, 10);
 
-    this.paymentService.authorizePayment(randomIdentifier, this.donationAmount, returnUrl).subscribe(response => {
-      window.open(response.formUrl, '_blank'); // Open the payment website in a new tab
-      localStorage.setItem('orderId', response.orderId);
-      this.orderId = response.orderId;
+    this.paymentService
+      .authorizePayment(randomIdentifier, this.donationAmount, returnUrl)
+      .subscribe(
+        (response) => {
+          window.open(response.formUrl, '_blank'); // Open the payment website in a new tab
+          localStorage.setItem('orderId', response.orderId);
+          this.orderId = response.orderId;
 
-      this.confirmPayment(response.orderId, this.donationAmount);
-    }, error => {
-      console.error('Authorization failed:', error);
-    });
+          this.confirmPayment(response.orderId, this.donationAmount);
+        },
+        (error) => {
+          console.error('Authorization failed:', error);
+        }
+      );
   }
 
   confirmPayment(orderId: string, amount: number): void {
-    this.paymentService.confirmPayment(orderId, amount).subscribe(response => {
-      if (this.selectedAssociation && this.selectedAssociation.id) {
-        console.log('Selected Association:', this.selectedAssociation);
-        const date = new Date();
-        this.paymentService.addDonAssociation(this.selectedAssociation.id, amount, date, this.donateurId)
-          .then(() => {
-            console.log('Don ajouté avec succès à la collection DonAssociation');
-            this.paymentSuccessful = localStorage.getItem('PaymentStatus');
-            console.log('confimed ' + this.paymentSuccessful);
-            console.log('Don ajouté avec succès à la collection');
-            if(this.orderStatus==2){
-              this.presentToast(`Votre don à ${this.selectedAssociation?.nom} a été transmis avec succès!`).then(() => {
-                window.close();
-              });
-            } else {
-              this.presentToast(`Votre don à ${this.selectedAssociation?.nom} a échoué. Veuillez vérifier l'état de votre carte!`).then(() => {
-                window.close();
-              });
-            }          
-          })
-          .catch(error => {
-            console.error('Erreur lors de l\'ajout du don à la collection :', error);
-          });
-      } else {
-        console.error('Erreur: Aucune association sélectionnée ou ID non défini.');
+    this.paymentService.confirmPayment(orderId, amount).subscribe(
+      (response) => {
+        if (this.selectedAssociation && this.selectedAssociation.id) {
+          console.log('Selected Association:', this.selectedAssociation);
+          const date = new Date();
+          this.paymentService
+            .addDonAssociation(
+              this.selectedAssociation.id,
+              amount,
+              date,
+              this.donateurId
+            )
+            .then(() => {
+              console.log(
+                'Don ajouté avec succès à la collection DonAssociation'
+              );
+              this.paymentSuccessful = localStorage.getItem('PaymentStatus');
+              console.log('confimed ' + this.paymentSuccessful);
+              console.log('Don ajouté avec succès à la collection');
+              if (this.orderStatus == 2) {
+                this.presentToast(
+                  `Votre don à ${this.selectedAssociation?.nom} a été transmis avec succès!`
+                ).then(() => {
+                  window.close();
+                });
+              } else {
+                this.presentToast(
+                  `Votre don à ${this.selectedAssociation?.nom} a échoué. Veuillez vérifier l'état de votre carte!`
+                ).then(() => {
+                  window.close();
+                });
+              }
+            })
+            .catch((error) => {
+              console.error(
+                "Erreur lors de l'ajout du don à la collection :",
+                error
+              );
+            });
+        } else {
+          console.error(
+            'Erreur: Aucune association sélectionnée ou ID non défini.'
+          );
+        }
+        console.log('Payment confirmed:', response);
+      },
+      (error) => {
+        console.error('Confirmation failed:', error);
       }
-      console.log('Payment confirmed:', response);
-    }, error => {
-      console.error('Confirmation failed:', error);
-    });
+    );
   }
 
   validateDonationAmount() {
@@ -157,13 +187,13 @@ export class AssociationDetailsPage implements OnInit {
       duration: 5000,
       position: 'top',
       animated: true,
-      color: 'dark'
+      color: 'dark',
     });
     toast.present();
     this.cdr.detectChanges();
   }
 
   @HostListener('swipeleft', ['$event']) public onSwipeLeft() {
-    this.router.navigate(['../']);  // navigate back
+    this.router.navigate(['../']); // navigate back
   }
 }
